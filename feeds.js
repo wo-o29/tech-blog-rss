8995cf132e87611259aaab4f31908553587112b6
wo-o29 <woogur29@gmail.com>
2024-12-01 22:35:36 +0900

    feat: 8시마다 rss feed 확인하고 새로 올라온게 있다면 메일 보내주기

---
+++ feeds.js
@@ -0,0 +1,71 @@
+const RSS_FEEDS = [
+  { name: "토스 블로그", url: "https://toss.tech/rss.xml" },
+  { name: "무신사 블로그", url: "https://medium.com/feed/musinsa-tech" },
+  { name: "네이버 블로그", url: "https://d2.naver.com/d2.atom" },
+  {
+    name: "마켓 컬리 블로그",
+    url: "https://helloworld.kurly.com/feed.xml",
+  },
+  {
+    name: "우아한 형제들 블로그",
+    url: "https://techblog.woowahan.com/feed/",
+  },
+  {
+    name: "카카오 엔터 블로그",
+    url: "https://tech.kakaoenterprise.com/feed",
+  },
+  {
+    name: "데브시스터즈 블로그",
+    url: "https://tech.devsisters.com/rss.xml",
+  },
+  {
+    name: "당근 블로그",
+    url: "https://medium.com/feed/daangn",
+  },
+  {
+    name: "쏘카 블로그",
+    url: "https://tech.socarcorp.kr/feed",
+  },
+  {
+    name: "뱅크샐러드 블로그",
+    url: "https://blog.banksalad.com/rss.xml",
+  },
+  {
+    name: "신은선님 블로그",
+    url: "https://eunsonny.github.io/rss.xml",
+  },
+  {
+    name: "정현수님 블로그",
+    url: "https://junghyeonsu.com/rss.xml",
+  },
+  {
+    name: "문동욱님 블로그",
+    url: "https://evan-moon.github.io/feed.xml",
+  },
+  {
+    name: "카카오 블로그",
+    url: "https://tech.kakao.com/blog/feed",
+  },
+  {
+    name: "정봉찬님 블로그",
+    url: "https://devwqc.github.io/rss.xml",
+  },
+  {
+    name: "지그재그님 블로그",
+    url: "https://wormwlrm.github.io/feed",
+  },
+  {
+    name: "테오님 블로그",
+    url: "https://api.velog.io/rss/@teo",
+  },
+  {
+    name: "손수림님 블로그",
+    url: "https://api.velog.io/rss/@surim014",
+  },
+  {
+    name: "왓에버 블로그",
+    url: "https://api.velog.io/rss/@whatever",
+  },
+];
+
+export default RSS_FEEDS;
---
+++ index.js
@@ -0,0 +1,90 @@
+import Parser from "rss-parser";
+import nodemailer from "nodemailer";
+import fs from "fs";
+import dotenv from "dotenv";
+import RSS_FEEDS from "./feeds.js";
+dotenv.config();
+
+const parser = new Parser({
+  timeout: 30000,
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    Accept:
+      "application/rss+xml, application/xml, application/atom+xml, text/xml, */*",
+  },
+});
+const LAST_CHECK_FILE = "last_check.json";
+
+// 이메일 전송을 위한 설정
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: "woogur29@gmail.com",
+    pass: process.env.APP_PASSWORD,
+  },
+});
+
+const sendMail = (fileData, feed, lastPost) => {
+  const mailOptions = {
+    from: "woogur29@gmail.com",
+    to: "woogur29@gmail.com",
+    subject: `${feed.name}의 새로운 글이 올라왔어요.`,
+    text: `${lastPost.title}\n${lastPost.link}`,
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      throw new Error(error);
+    }
+
+    console.log("메일이 정상적으로 전송되었습니다.");
+    writeBlogData(fileData, feed.name, lastPost.title);
+  });
+};
+
+const writeBlogData = (fileData, feedName, lastPostTitle) => {
+  const copyFileData = { ...fileData };
+  copyFileData[feedName] = lastPostTitle;
+  fs.writeFileSync(LAST_CHECK_FILE, JSON.stringify(copyFileData));
+};
+
+const getFileData = () => {
+  if (fs.existsSync(LAST_CHECK_FILE)) {
+    const fileData = fs.readFileSync(LAST_CHECK_FILE).toString();
+    return JSON.parse(fileData);
+  }
+};
+
+const convertJSON = (data) => {
+  const string = JSON.stringify(data);
+  return JSON.parse(string);
+};
+
+const checkRSSFeeds = async () => {
+  console.log("체크 시작");
+  const fileData = getFileData();
+
+  for (const feed of RSS_FEEDS) {
+    try {
+      const parsedFeed = await parser.parseURL(feed.url); // rss 내용
+      const lastPost = convertJSON(parsedFeed.items[0]);
+
+      if (!fileData[feed.name]) {
+        // 처음 추가한 RSS 데이터인 경우
+        writeBlogData(fileData, feed.name, lastPost.title);
+        fileData[feed.name] = lastPost.title;
+      }
+
+      if (fileData[feed.name] !== lastPost.title) {
+        sendMail(fileData, feed, lastPost);
+      }
+    } catch (error) {
+      console.error(`Error checking RSS feed ${feed.name}:`, error);
+    }
+  }
+};
+
+checkRSSFeeds();
